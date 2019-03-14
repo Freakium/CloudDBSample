8ec2180cd805ac42384f5add8b91ace7145281c6
unknown <rosslcchan@gmail.com>
2019-03-13 21:18:51 -0600

    - Added integration and querying functionality - Spiced up the webpage with colours

--- public/functions.js
+++ public/functions.js
@@ -1,18 +1,64 @@
-var fileUpload = function() {
-    // displays error message to page
-    function displayMessage(message, err) {
-        var mode;
-        if(err)
-            mode = 'alert-danger';
-        else
-            mode = 'alert-success';
+// displays error message to page
+var displayMessage = function(message, err) {
+    var mode;
+    if(err)
+        mode = 'alert-danger';
+    else
+        mode = 'alert-success';
 
-        document.getElementById('messages').innerHTML =
-            '<div class="alert ' + mode + ' alert-dismissible fade show" role="alert">' +
-            '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' + message;
+    console.log(message);
+
+    document.getElementById('messages').innerHTML =
+        '<div class="alert ' + mode + ' alert-dismissible fade show" role="alert">' +
+        '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' + message;
+}
+
+// Uploads SQL .db file and integrates its data
+var fileUpload = function() {
+    function uploadFile(filename, data) {
+        $('#waiting').show();
+        $.ajax({
+            url: "/",
+            type: "POST",
+            data: data,
+            enctype: 'multipart/form-data',
+            processData: false,
+            contentType: false,
+            success: function() {
+                displayMessage(filename + ' successfully integrated.', false);
+                $('#waiting').hide();
+            },
+            error: function(e) {
+                displayMessage('An error occurred: ' + e.responseText, true);
+                console.error(e);
+                $('#waiting').hide();
+            }
+        });
     }
+    return {
+        dbUploadHandler: function(file) {
+            // validation
+            document.getElementById('uploadFilename').innerHTML = file[0].name;
+            document.getElementById('messages').innerHTML = "";
+
+            // Make sure uploaded file has .db extension
+            if(file[0].name.split('.').pop() == "db") {
+                var data = new FormData();
+                data.append('file', file[0]);
+                uploadFile(file[0].name, data);
+            }
+            else {
+                displayMessage(file[0].name + ' is not a valid (.db) file.', true);
+            }
+        },
+    };
+}();
+
+// Queries the database
+var dbQuery = function() {
+    // displays query results
     function displayQuery(data) {
-        var queryContent = '<h4>SQL Query Preview</h4><table class="dbQuery"><tr>';
+        var queryContent = '<h4>Query Result</h4><table class="dbQuery"><tr>';
         
         if(data !== null) {
             var keys = Object.keys(data[0]);
@@ -31,38 +77,32 @@ var fileUpload = function() {
         }
         document.getElementById('dbContentPane').innerHTML = queryContent;
     }
+    function queryDatabase(data) {
+        var query = {query: data};
+        $.ajax({
+            url: "/queries",
+            type: "POST",
+            data: JSON.stringify(query),
+            contentType: 'application/json',
+            success: function(data) {
+                displayQuery(data);
+            },
+            error: function(e) {
+                displayMessage('An error occurred: ' + e.responseText, true);
+                console.error(e);
+            }
+        });
+    }
     return {
-        dbUploadHandler: function(file) {
-            // validation
-            document.getElementById('uploadFilename').innerHTML = file[0].name;
-            document.getElementById('messages').innerHTML = "";
-
-            // Make sure uploaded file has .db extension
-            if(file[0].name.split('.').pop() == "db") {
-                var data = new FormData();
-                data.append('file', file[0]);
-
-                $.ajax({
-                    url: "/",
-                    type: "POST",
-                    data: data,
-                    enctype: 'multipart/form-data',
-                    processData: false,
-                    contentType: false,
-                    success: function(data) {
-                        displayMessage(file[0].name + ' successfully uploaded.', false);
-                        displayQuery(data);
-                    },
-                    error: function(e) {
-                        displayMessage('An error occurred: ' + e.responseText, true);
-                        console.log(e);
-                    }
-                });
-                //$('#dbContentPane').show();
+        submitQuery: function(data) {
+            var query;
+            if(data == 'sqlQuery') {
+                query = document.getElementById('sqlQuery').value;
             }
             else {
-                displayMessage(file[0].name, true);
+                query = 'SELECT * FROM ' + data;
             }
+            queryDatabase(query);
         }
-    };
+    }
 }();
--- server.js
+++ server.js
@@ -1,53 +1,137 @@
 const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
+const bodyParser = require('body-parser');
 const multer = require('multer');
 const upload = multer();
 const fs = require('fs');
 const app = express();
-var output;
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render('index', {dbContents:null});
+    res.render('pages/index', {dbContents:null});
+});
+app.get('/queries', function(req, res) {
+    res.render('pages/queries');
 });
 
+// Integration post route
 app.post('/', upload.any(), (req, res) => {
-    output = [];
     fs.writeFile('./databases/temp/' + req.files[0].originalname, req.files[0].buffer, (err) => {
         if (err) {
-            console.log('Error: ', err);
             res.status(500).send('An error occurred: ' + err);
-        } else {
-            let db = new sqlite3.Database('./databases/temp/' + req.files[0].originalname, sqlite3.OPEN_READWRITE, (err) => {
+            console.error('Error: ', err);
+        }
+        else {
+            let db = new sqlite3.Database('./databases/primary.db', sqlite3.OPEN_READWRITE, (err) => {
                 if (err) {
-                    console.error(err.message);
+                    res.status(500).send(err);
+                    console.error(err);
                 }
                 console.log('Connected to the database.');
             });
-            /*db.run(`ATTACH './databases/` + req.files[0].originalname + `' AS secondary;
-                INSERT INTO Tool SELECT * FROM secondary.Tool;
-                DETACH secondary;`);*/
-            db.serialize(() => {
-                db.each('SELECT * FROM Tool', (err, row) => {
-                    if (err) {
-                        console.error(err.message);
-                    }
-                    output.push(row);
+            let secondary = new sqlite3.Database('./databases/temp/' + req.files[0].originalname, sqlite3.OPEN_READWRITE, (err) => {
+                if (err) {
+                    res.status(500).send(err);
+                    console.error(err);
+                }
+                console.log('Connected to the secondary database.');
+            });
+            
+            // Get all tables from uploaded DB and integrate
+            secondary.each(`SELECT name FROM sqlite_master WHERE type='table'`, (error, tableRow) => {
+                if (error) {
+                    res.status(500).send(error);
+                    console.error(error);
+                }
+                Object.keys(tableRow).forEach((table) => {
+                    secondary.each('SELECT * FROM ' + tableRow[table], (err, row) => {
+                        if (err) {
+                            res.status(500).send(err);
+                            console.error(err);
+                        }
+                        // Collects column names
+                        const keys = Object.keys(row);
+                        const columns = keys.toString();
+                        let values = '';
+                
+                        // Collects values
+                        Object.keys(row).forEach((r) => {
+                            values += ",'" + row[r] + "'";
+                        });
+                        values = values.substr(1);
+        
+                        db.run('INSERT OR IGNORE INTO '+ tableRow[table] +' ('+columns+') VALUES ('+values+')');
+                    });
                 });
             });
-            db.close((err) => {
+            
+            // close the database connections and delete secondary db file
+            secondary.close((err) => {
                 if (err) {
-                    console.error(err.message);
+                    res.status(500).send(err);
+                    console.error(err);
                 }
-                else {
-                    res.status(200).send(output);
-                    console.log('Close the database connection.');
-                }
+                console.log('Closed the secondary database connection.');
+
+                // delete temporary 2nd db
+                fs.unlink('./databases/temp/' + req.files[0].originalname, function (err) {
+                    if (err) {
+                        res.status(500).send(err);
+                        console.error(err);
+                    }
+                    console.log(req.files[0].originalname + ' deleted!');
+                });
+                db.close((err) => {
+                    if (err) {
+                        res.status(500).send(err);
+                        console.error(err);
+                    }
+                    else {
+                        res.status(200).send('ok');
+                        console.log('Closed the primary database connection.');
+                    }
+                });
             });
         }
     });
+});
+
+app.post('/queries', (req, res) => {
+    console.log(req.body.query);
+    // test if query begins with SELECT statement
+    var queryTest = req.body.query.split(' ');
+    if(queryTest[0] != 'SELECT') {
+        res.status(500).send('Please query database with a SELECT statement.');
+    }
+    else {
+        var output = [];
+        let db = new sqlite3.Database('./databases/primary.db', sqlite3.OPEN_READWRITE, (err) => {
+            if (err) {
+                res.status(500).send(err);
+                console.error(err);
+            }
+            console.log('Connected to the database.');
+        });
+        db.each(req.body.query, (err, row) => {
+            if (err) {
+                res.status(500).send(err);
+                console.error(err);
+            }
+            output.push(row);
+        }).close((err) => {
+            if (err) {
+                res.status(500).send(err);
+                console.error(err);
+            }
+            else {
+                res.status(200).send(output);
+                console.log('Closed the primary database connection.');
+            }
+        });
+    }
 });
 
 app.listen(8080, function() {
